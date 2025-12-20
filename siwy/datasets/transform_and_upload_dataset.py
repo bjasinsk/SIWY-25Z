7e65a0a66724dca4ebf5947aa75d64bf3e3d9826
Marcin Jarczewski <marcin.jarc@gmail.com>
2025-12-21 00:27:00 +0100

    update transform

--- siwy/datasets/transform_and_upload_dataset.py
+++ siwy/datasets/transform_and_upload_dataset.py
@@ -29,10 +29,10 @@ DATASETS = [
 
 DEFAULT_TRANSFORM = transforms.Compose(
     [
-        transforms.RandomCrop(224, pad_if_needed=True, fill=0),
-        transforms.RandomCrop(224),
+        transforms.Resize(256),
+        transforms.CenterCrop(224),
         transforms.ToTensor(),
-        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.201)),
     ]
 )
 
